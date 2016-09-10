1f6f0520e6ca2ecfe256d8562544ca2a419d5dd0
Nishtha <nishtha.gakkhar@cognizant.com>
2016-09-10 11:06:18 +0530

    email already exist feature added

--- public/js/service/register.service.js
+++ public/js/service/register.service.js
@@ -1,18 +1,20 @@
 angular.module('portfolio')
-.service('register', function($http,$window,$location) {
-  this.newUser = function(username,email,password) {
+.service('register', function($http,$window,$location,$mdDialog) {
+  this.newUser = function(username,email,password){
     console.log("post");
     var registerCredentials = { "username": username,"email":email,"password":password };
 
-    var user= $http.get('/register/'+registerCredentials.email);
-    // .success(function(user){
+    var user= $http.get('/register/'+registerCredentials.email).success(function(user){
       if(user.email == registerCredentials.email){
         alert("email existed already");
+        $mdDialog.cancel();
         return 1;
       }
       else {
         return $http.post('/register',registerCredentials);
+        $scope.authUser=user;
+        $mdDialog.cancel();
       }
-    // });
+     });
   };
 });
